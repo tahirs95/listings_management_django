58d5c727f0d5383b72b5a7cb03a598570783833d
Sheikh Abdul Wahid Ahmed <sheikhG1900@gmail.com>
2021-05-23 22:54:12 +0500

    booking views done.

--- listings/views.py
+++ listings/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from django.db.models import Q, OuterRef, Subquery, Count, F
+from django.db.models import Q, OuterRef, Subquery, Count, F, functions 
 from .models import BookingInfo, Listing, BlockDay, HotelRoom, HotelRoomType
 
 @csrf_exempt
@@ -22,28 +22,27 @@ def get_listings(request):
     if check_out:
         check_out_obj = datetime.strptime(check_out, '%Y-%m-%d')
     
-    block_days = BookingInfo.objects.all()
-    a = BookingInfo.objects.all()
-    b = BookingInfo.objects.get(id=4)
-    print(b.block_days)
-    # infos = BookingInfo.objects.filter(~Q(block_days__check_in__lt = datetime.now()))
-    # qs_hotal_rooms_count = HotelRoom.objects.filter(hotel_room_type = OuterRef('pk')).annotate(room_count = Count('pk'))
-    reserved_room_count = BlockDay.objects.filter(booking__hotel_room_type = OuterRef('pk')).annotate(blocked_count = Count('booking__hotel_room_type')).values('blocked_count')
-    testing = HotelRoomType.objects.annotate(rooms_count = Count('hotel_rooms'), blocked_count = Count('booking_info__block_days'))
-    # .filter(rooms_count__gt = Subquery(reserved_room_count))
-    print(testing)
-    infos = BookingInfo.objects.filter(block_days__check_in__lt = datetime.now())
+    qs_reserved_room_count = BlockDay.objects \
+        .filter(booking = OuterRef('pk')) \
+        .values('booking') \
+        .annotate(reserved_count = Count('*')) \
+        .values('reserved_count') \
+
+    available_bookigs = BookingInfo.objects \
+        .annotate(rooms_count = Count('hotel_room_type__hotel_rooms'), reserved_count = functions.Coalesce(Subquery(qs_reserved_room_count), 0)) \
+        .filter(Q(rooms_count__gt = F('reserved_count')) | Q(Q(listing__isnull = False) & Q(reserved_count = 0)))
+
     items = []
-    for info in testing:
+    for booking in available_bookigs:
         items.append({
             # "listing_type": info.hotel_room_type.hotel.listing_type,
             # "title": info.hotel_room_type.title,
             # "country": info.hotel_room_type.hotel.country,
             # "city": info.hotel_room_type.hotel.city,
-            "rooms_count": info.rooms_count,
-            "blocked_count": info.blocked_count,
-            "title": info.title
-
+            "rooms_count": booking.rooms_count,
+            "reserved_count": booking.reserved_count,
+            "title": booking.hotel_room_type.title if booking.hotel_room_type else booking.listing.title,
+            "price": booking.price
         })
     data = {'items':items}
     return Response(data)
